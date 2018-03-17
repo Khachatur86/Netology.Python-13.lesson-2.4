353dc380281d3d507ac55103427f1716fd6ae360
Хачатур <>
2018-03-17 11:32:12 +0300

    Work done

--- lesson2.4.py
+++ lesson2.4.py
@@ -1,17 +1,34 @@
 
+"""Пользователь вводит поисковую информацию. Программа считывает эту информацию, ищет наличие этой информации в файлах
+папки Migrations. Если находит их, то в заранее созданный пустой список дополняет файлами, названия которых совпали.
+Далее выводим наименования файлов и их количество (len) в списке(который был до этого пустым)"""
 import os
+# put abspath for Migration directory
+def get_abspath(directory_name = "Migrations"):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    migration_abs_path = os.path.join(current_directory, directory_name)
+    return migration_abs_path
 
+# creating function of empty file list, that append with input data
+def creating_file_list(input_info, files_list, migration_abs_path = get_abspath()):
+    empty_file_list = []
+    for file in files_list:
+        with open(os.path.join(migration_abs_path, file), 'rt') as f:
+            data = f.read()
+            if input_info.lower() in data.lower():
+                empty_file_list.append(file)
+    return empty_file_list, len(empty_file_list)
 
-migrations = "Migrations"
-current_directory = os.path.dirname(os.path.abspath(__file__))
-migration_abs_path = os.path.join(current_directory, migrations)
-files_list = [f for f in os.listdir(migration_abs_path) if f.endswith('.sql')]
+# Main part
+def core():
+    migration_abs_path = get_abspath()
+    files_list = [f for f in os.listdir(migration_abs_path) if f.endswith('.sql')]
 
-rekurs_files = []
-user_input = input("Введите строку: ")
-for file in files_list:
-    with open(os.path.join(migration_abs_path, file), 'rt') as f:
-        data_text = f.read()
-        if user_input.lower() in data_text.lower():
-            rekurs_files.append[file]
-
+    while True:
+        search_info = input('Введите строку: ')
+        file_list_info = creating_file_list(search_info, files_list, migration_abs_path)
+        files, quantity = file_list_info
+        for file in files:
+            print(file)
+        print('Всего: {0}'.format(quantity))
+core()
